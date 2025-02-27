0413c5999adc891403506e50a4e09624f6e50297
Nish Patel <nish14062003@gmail.com>
2025-02-27 14:31:15 +0530

    UI changes

--- client/expensify/app/login/page.tsx
+++ client/expensify/app/login/page.tsx
@@ -1,41 +1,26 @@
 "use client";
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@apollo/client";
 import { useRouter } from "next/navigation";
 import { SIGNIN } from "@/graphql/mutations";
 import Cookies from "js-cookie";
 import Link from "next/link";
-import { EyeIcon, EyeOffIcon, Calendar } from "lucide-react";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { motion } from "framer-motion";
+import Image from "next/image";
+
+import handWave from "@/public/hand_wave.png";
+import mailIcon from "@/public/mail_icon.svg";
+import lockIcon from "@/public/lock_icon.svg";
 
 interface LoginFormInputs {
   email: string;
   password: string;
 }
 
-const containerVariants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.8,
-      when: "beforeChildren",
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-};
-
 const Login: React.FC = () => {
   const [signin, { loading, error }] = useMutation(SIGNIN);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const {
@@ -46,182 +31,137 @@ const Login: React.FC = () => {
 
   const onSubmit = async (formData: LoginFormInputs) => {
     try {
-      console.log("Login Data:", formData);
       const response = await signin({
         variables: { email: formData.email, password: formData.password },
       });
 
       if (response.data) {
-        console.log("Login Response:", response.data.signin.token);
         Cookies.set("token", response.data.signin.token, {
           secure: true,
           sameSite: "strict",
         });
-        setSuccessMessage("You are successfully logged in!");
+        router.push("/home");
       }
     } catch (err) {
       console.error("Login Error:", err);
-      setSuccessMessage(null);
     }
   };
 
-  useEffect(() => {
-    if (successMessage) {
-      {
-        router.push("/home");
-      }
-    }
-  }, [successMessage, router]);
-
   return (
-    <div className="flex h-screen bg-gray-50">
+    <motion.div
+      className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-gray-200 to-blue-400"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <motion.div
-        className="w-1/2 flex justify-center items-center bg-teal-600"
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
+        className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-colour text-sm"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
       >
-        <img
-          src="/image_login.svg"
-          alt="Login Illustration"
-          className="w-full h-full object-cover"
-        />
-      </motion.div>
-
-      <motion.div
-        className="w-1/2 flex justify-center items-center overflow-hidden"
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <motion.div
-          className="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md mx-4"
-          variants={containerVariants}
-          initial="hidden"
-          animate="visible"
+        <motion.h2
+          className="text-white text-2xl font-bold text-center mb-6 flex items-center justify-center gap-2"
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
         >
-          <motion.h2
-            className="flex items-center justify-center text-2xl font-bold text-teal-600 mb-6"
-            variants={itemVariants}
-          >
-            <Calendar className="w-6 h-6 mr-2" />
-            Welcome Back!
-          </motion.h2>
-
-          {loading && (
-            <motion.p
-              className="text-teal-500 text-center"
-              variants={itemVariants}
-            >
-              Loading...
-            </motion.p>
-          )}
-          {error && (
-            <motion.p
-              className="text-red-500 text-center"
-              variants={itemVariants}
-            >
-              {error.message}
-            </motion.p>
-          )}
-          {successMessage && (
-            <motion.p
-              className="text-green-500 text-center"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-            >
-              {successMessage}
-            </motion.p>
-          )}
-
-          <motion.form
-            onSubmit={handleSubmit(onSubmit)}
-            className="space-y-5 mt-5"
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-          >
-            <motion.div variants={itemVariants}>
-              <input
-                type="email"
-                placeholder="Email"
-                className={`w-full px-4 py-2 border rounded-lg focus:outline-none transition-colors ${
-                  errors.email
-                    ? "border-red-500"
-                    : "border-gray-300 hover:border-teal-400"
-                }`}
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /^\S+@\S+\.\S+$/,
-                    message: "Enter a valid email address",
-                  },
-                })}
-              />
-              {errors.email && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.email.message}
-                </p>
-              )}
-            </motion.div>
-
-            <motion.div className="relative" variants={itemVariants}>
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                className={`w-full px-4 py-2 border rounded-lg focus:outline-none transition-colors ${
-                  errors.password
-                    ? "border-red-500"
-                    : "border-gray-300 hover:border-teal-400"
-                }`}
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 6,
-                    message: "Password must be at least 6 characters",
-                  },
-                })}
-              />
-              <button
-                type="button"
-                className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? (
-                  <EyeOffIcon className="w-5 h-5" />
-                ) : (
-                  <EyeIcon className="w-5 h-5" />
-                )}
-              </button>
-              {errors.password && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.password.message}
-                </p>
-              )}
-            </motion.div>
-
-            <motion.button
-              type="submit"
-              className="w-full bg-teal-600 text-white py-2 rounded-lg hover:bg-teal-500 transition transform hover:scale-105"
-              disabled={loading}
-              variants={itemVariants}
-            >
-              {loading ? "Logging in..." : "Login"}
-            </motion.button>
-          </motion.form>
-
+          Welcome Back
+          <Image src={handWave} alt="Wave" width={30} height={30} />
+        </motion.h2>
+        {error && (
           <motion.p
-            className="text-center text-teal-600 mt-6"
-            variants={itemVariants}
+            className="text-red-500 text-center bg-gray-700 p-2 rounded-md"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
           >
-            Don&apos;t have an account?{" "}
-            <Link href="/auth/signup" className="text-teal-800 font-medium">
-              Sign up
-            </Link>
+            {error.message}
           </motion.p>
-        </motion.div>
+        )}
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+          <motion.div
+            className="relative"
+            initial={{ x: -30, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <input
+              type="email"
+              placeholder="Email"
+              className="mb-4 flex items-center gap-3 w-full px-12 py-2.5 rounded-full bg-[#5C6691]"
+              {...register("email", { required: "Email is required" })}
+            />
+            <Image
+              src={mailIcon}
+              alt="Email"
+              width={20}
+              height={20}
+              className="absolute left-4 top-3 text-gray-400"
+            />
+            {errors.email && (
+              <p className="text-red-400 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </motion.div>
+          <motion.div
+            className="relative"
+            initial={{ x: 30, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
+          >
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="mb-4 flex items-center gap-3 w-full px-12 py-2.5 rounded-full bg-[#5C6691]"
+              {...register("password", { required: "Password is required" })}
+            />
+            <Image
+              src={lockIcon}
+              alt="Lock"
+              width={15}
+              height={15}
+              className="absolute left-4 top-3 text-gray-400 "
+            />
+            <button
+              type="button"
+              className="absolute right-3 top-3 text-gray-400 hover:text-white"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <EyeOffIcon className="w-5 h-5" />
+              ) : (
+                <EyeIcon className="w-5 h-5" />
+              )}
+            </button>
+          </motion.div>
+          <motion.button
+            type="submit"
+            className="text-white font-medium w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900"
+            disabled={loading}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            {loading ? "Logging in..." : "Login"}
+          </motion.button>
+        </form>
+        <motion.p
+          className="text-gray-400 text-center text-xs mt-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.3 }}
+        >
+          Already a member?{" "}
+          <Link
+            href="/signup"
+            className="text-blue-400 cursor-pointer underline"
+          >
+            Sign up
+          </Link>
+        </motion.p>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
